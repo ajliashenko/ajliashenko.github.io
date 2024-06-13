4df099300c5ca16f543dd3c3a2e7a7719a911657
ajliashenko <anastasia.liashenko3@gmail.com>
2024-06-13 17:05:40 -0400

    added in a how section
    yaaaay <3

--- docs/medvedska.js
+++ docs/medvedska.js
@@ -62,6 +62,14 @@ document.getElementById('applyButton').addEventListener('click', function() {
     window.location.href = mailtoLink;
 });
 
+document.getElementById('contactButton').addEventListener('click', function() {
+    const subject = "Consultation";
+    const body = "Please use this template: \n \n\nYour name:\nCompany name: \nCity:\nShort term/long term goals:\n\n\nCurrent pain points or roadblocks:\n\nTime-line when you will like things completed:\n ";
+    const mailtoLink = `mailto:Aj@medvedska.com?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
+
+    window.location.href = mailtoLink;
+});
+
 document.addEventListener('DOMContentLoaded', function() {
     // Force reflow
     document.body.style.overflow = 'hidden';
@@ -70,6 +78,8 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // Other existing JavaScript code here...
 });
+
+
 /*(function() {
     function getCookie(name) {
         let match = document.cookie.match(new RegExp('(^| )' + name + '=([^;]+)'));
@@ -105,3 +115,40 @@ document.addEventListener('DOMContentLoaded', function() {
     window.acceptCookies = acceptCookies;
     window.declineCookies = declineCookies;
 })();*/
+// Makeshift carousel function that gets invoked with the Index to start it off, then the callback increments the index to recursively invoke the same function. Works even in IE11!
+// Makeshift carousel function that gets invoked with the Index to start it off, then the callback increments the index to recursively invoke the same function. Works even in IE11!
+var testimonialItems = document.querySelectorAll(".item label");
+var timer;
+function cycleTestimonials(index) {
+   timer = setTimeout(function() {
+    var evt;
+    if (document.createEvent){
+      //If browser = IE, then polyfill
+      evt = document.createEvent('MouseEvent');
+      evt.initMouseEvent('click', true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+    } else {
+      //If Browser = modern, then create new MouseEvent
+      evt = new MouseEvent("click", {
+            view: window,
+            bubbles: true,
+            cancelable: true,
+            clientX: 20
+          });
+    }
+    var ele = "." + testimonialItems[index].className;
+    var ele2 = document.querySelector(ele)
+    ele2.dispatchEvent(evt);
+    index++; // Increment the index
+    if (index >= testimonialItems.length) {
+      index = 0; // Set it back to `0` when it reaches `3`
+    }
+    cycleTestimonials(index); // recursively call `cycleTestimonials()`
+    document.querySelector(".testimonials").addEventListener("click", function() {
+      clearTimeout(timer); //stop the carousel when someone clicks on the div
+    });
+  }, 2000); //adjust scroll speed in miliseconds
+}
+//run the function
+cycleTestimonials(0);
+
+document.getElementById('image_profile').innerHTML = '<img style="width:100%" src="avatar_hat.jpg" />';
